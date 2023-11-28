823267810f7a26e1f89d7c765fc65dc5a03440c1
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-11-28 09:06:33 +0000

    Added second part, DayOfYear, to exercise

---
+++ src/main/java/refactoring/DayOfYear.java
@@ -0,0 +1,43 @@
+package refactoring;
+
+/**
+ * Smelly Example #1
+ *
+ * https://web.mit.edu/6.005/www/fa16/classes/04-code-review/
+ *
+ * Collaboratively authored with contributions from: Saman Amarasinghe, Adam
+ * Chlipala, Srini Devadas, Michael Ernst, Max Goldman, John Guttag, Daniel
+ * Jackson, Rob Miller, Martin Rinard, and Armando Solar-Lezama.
+ *
+ * This work is licensed under CC BY-SA 4.0.
+ */
+public class DayOfYear {
+
+    public static int dayOfYear(int month, int dayOfMonth, int year) {
+        if (month == 2) {
+            dayOfMonth += 31;
+        } else if (month == 3) {
+            dayOfMonth += 59;
+        } else if (month == 4) {
+            dayOfMonth += 90;
+        } else if (month == 5) {
+            dayOfMonth += 31 + 28 + 31 + 30;
+        } else if (month == 6) {
+            dayOfMonth += 31 + 28 + 31 + 30 + 31;
+        } else if (month == 7) {
+            dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30;
+        } else if (month == 8) {
+            dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31;
+        } else if (month == 9) {
+            dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+        } else if (month == 10) {
+            dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+        } else if (month == 11) {
+            dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+        } else if (month == 12) {
+            dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 31;
+        }
+
+        return dayOfMonth;
+    }
+}
